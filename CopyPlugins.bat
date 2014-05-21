4bad8265d0e691ed0ca8e9d260686873682ccdbb
Bojan Antolovic <bojan.antolovic@gmail.com>
2014-05-21 13:29:22 +0200

    Improved build scripts.

--- CopyPlugins.bat
+++ CopyPlugins.bat
@@ -1,10 +1,10 @@
 ECHO Target folder = [%1]
 ECHO $(ConfigurationName) = [%2]
 
-SET ThisScriptFolder="%~dp0"
+REM "%~dp0" is this script's folder.
 
-XCOPY /Y/D/R %ThisScriptFolder%Plugins\Rhetos.RestGenerator\bin\%2\Rhetos.RestGenerator.dll %1 || EXIT /B 1
-XCOPY /Y/D/R %ThisScriptFolder%Plugins\Rhetos.RestGenerator\bin\%2\Rhetos.RestGenerator.pdb %1 || EXIT /B 1
-XCOPY /Y/D/R %ThisScriptFolder%Packages\Newtonsoft.Json.6.0.1\lib\net40\Newtonsoft.Json.dll %1 || EXIT /B 1
+XCOPY /Y/D/R "%~dp0"Plugins\Rhetos.RestGenerator\bin\%2\Rhetos.RestGenerator.dll %1 || EXIT /B 1
+XCOPY /Y/D/R "%~dp0"Plugins\Rhetos.RestGenerator\bin\%2\Rhetos.RestGenerator.pdb %1 || EXIT /B 1
+XCOPY /Y/D/R "%~dp0"Packages\Newtonsoft.Json.6.0.1\lib\net40\Newtonsoft.Json.dll %1 || EXIT /B 1
 
 EXIT /B 0
