0a118dcf9f8afd2abe14fdc590355ce8a5f5b8d9
Bojan Antolović <bojan.antolovic@gmail.com>
2017-06-30 11:12:46 +0200

    Release 2.3.0.

--- ChangeVersions.bat
+++ ChangeVersions.bat
@@ -1,7 +1,7 @@
 @SETLOCAL
 @SETLOCAL ENABLEDELAYEDEXPANSION
 @REM //////////////////////////////////////////////////////
-SET BuildVersion=2.3.0
+SET BuildVersion=2.4.0
 SET PrereleaseVersion=auto
 @REM SET PrereleaseVersion TO EMPTY VALUE FOR THE OFFICIAL RELEASE.
 @REM SET PrereleaseVersion TO "auto" FOR AUTOMATIC PRERELEASE NAME "alpha<date and time><last commit hash>"
