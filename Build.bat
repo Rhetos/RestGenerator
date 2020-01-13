7a1e5adebc06ec29574102d9182085b523b60759
Bojan Antolović <bojan.antolovic@gmail.com>
2020-01-13 15:39:27 +0100

    Setting build script ExecutionPolicy to not depend on build machine configuration.

--- Build.bat
+++ Build.bat
@@ -5,7 +5,7 @@ SET Prerelease=auto
 CALL Tools\Build\FindVisualStudio.bat || GOTO Error0
 
 REM Updating the build version.
-PowerShell .\ChangeVersion.ps1 %Version% %Prerelease% || GOTO Error0
+PowerShell -ExecutionPolicy ByPass .\ChangeVersion.ps1 %Version% %Prerelease% || GOTO Error0
 WHERE /Q NuGet.exe || ECHO ERROR: Please download the NuGet.exe command line tool. && GOTO Error0
 NuGet restore -NonInteractive || GOTO Error0
 MSBuild /target:rebuild /p:Configuration=Debug /verbosity:minimal /fileLogger || GOTO Error0
@@ -13,7 +13,7 @@ IF NOT EXIST Install md Install
 NuGet pack -OutputDirectory Install || GOTO Error0
 
 REM Updating the build version back to "dev" (internal development build), to avoid spamming git history with timestamped prerelease versions.
-PowerShell .\ChangeVersion.ps1 %Version% dev || GOTO Error0
+PowerShell -ExecutionPolicy ByPass .\ChangeVersion.ps1 %Version% dev || GOTO Error0
 
 @REM ================================================
 
