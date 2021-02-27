489feb3241ffe341b6fdce3be695fe38a5b2db13
Bojan Antolović <bojan.antolovic@gmail.com>
2021-02-27 14:53:34 +0100

    Error logging.

--- src/Rhetos.Host.AspNet.RestApi/Filters/ApiExceptionFilter.cs
+++ src/Rhetos.Host.AspNet.RestApi/Filters/ApiExceptionFilter.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Logging;
 using Rhetos.Host.AspNet.RestApi.Utilities;
 
 namespace Rhetos.Host.AspNet.RestApi.Filters
@@ -11,11 +12,14 @@ namespace Rhetos.Host.AspNet.RestApi.Filters
     public class ApiExceptionFilter : IActionFilter, IOrderedFilter
     {
         private readonly JsonErrorHandler jsonErrorHandler;
+        private readonly ILogger logger;
+
         public int Order { get; } = int.MaxValue - 10;
 
-        public ApiExceptionFilter(JsonErrorHandler jsonErrorHandler)
+        public ApiExceptionFilter(JsonErrorHandler jsonErrorHandler, ILogger<ApiExceptionFilter> logger)
         {
             this.jsonErrorHandler = jsonErrorHandler;
+            this.logger = logger;
         }
 
         public void OnActionExecuting(ActionExecutingContext context)
@@ -30,7 +34,7 @@ namespace Rhetos.Host.AspNet.RestApi.Filters
             var invalidModelEntry = invalidModelEntries.First();
             var errors = string.Join("\n", invalidModelEntry.Value.Errors.Select(a => a.ErrorMessage));
 
-            // if no key is present, it means there is an error deserializing body
+            // If no key is present, it means there is an error deserializing body.
             
             if (string.IsNullOrEmpty(invalidModelEntry.Key))
             {
@@ -57,7 +61,25 @@ namespace Rhetos.Host.AspNet.RestApi.Filters
                 var (response, statusCode) = jsonErrorHandler.CreateResponseFromException(context.Exception);
                 context.Result = new JsonResult(response) { StatusCode = statusCode };
                 context.ExceptionHandled = true;
+                LogException(context.Exception);
             }
+        }
+
+        private void LogException(Exception error)
+        {
+            if (error is UserException)
+                logger.LogTrace(error.ToString());
+            else if (error is LegacyClientException legacyException)
+            {
+                if (legacyException.Severe)
+                    logger.LogInformation(legacyException.ToString());
+                else
+                    logger.LogTrace(legacyException.ToString());
+            }
+            else if (error is ClientException)
+                logger.LogInformation(error.ToString());
+            else
+                logger.LogError(error.ToString());
         }
     }
 }
