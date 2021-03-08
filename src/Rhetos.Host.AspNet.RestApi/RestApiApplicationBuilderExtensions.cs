4d61e36e225a466e10a667bf301c56e22fe676e5
Sasa Stublic <sasa.stublic@gmail.com>
2021-03-08 17:55:00 +0100

    Improved RestApi extensions so that they can be called multiple times and intantiating RhetosHost is deferred as much as possible. Introduced '.UseRhetosRestApi()' extension method.

---
+++ src/Rhetos.Host.AspNet.RestApi/RestApiApplicationBuilderExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Rhetos;
+using Rhetos.Host.AspNet.RestApi;
+using Rhetos.Host.AspNet.RestApi.Metadata;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class RestApiApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseRhetosRestApi(this IApplicationBuilder app)
+        {
+            var controllerRestInfoRepository = app.ApplicationServices.GetService<ControllerRestInfoRepository>();
+
+            if (controllerRestInfoRepository == null)
+            {
+                throw new InvalidOperationException(
+                    $"Can't resolve {nameof(ControllerRestInfoRepository)} from dependency injection. Did you forget to 'AddRhetos().AddRestApi()' in ConfigureServices of Startup.cs file?");
+            }
+
+            var mvcOptions = app.ApplicationServices.GetRequiredService<IOptions<MvcOptions>>();
+            var restApiOptions = app.ApplicationServices.GetRequiredService<IOptions<RestApiOptions>>();
+            mvcOptions.Value.Conventions.Add(new RestApiControllerRouteConvention(restApiOptions.Value, controllerRestInfoRepository));
+
+            var applicationPartManager = app.ApplicationServices.GetRequiredService<ApplicationPartManager>();
+            applicationPartManager.FeatureProviders.Add(new RestApiControllerFeatureProvider(controllerRestInfoRepository));
+
+            return app;
+        }
+    }
+}
