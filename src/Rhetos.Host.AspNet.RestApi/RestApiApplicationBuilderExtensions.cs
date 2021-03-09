b99d744149d40e5b1a190ea6be623ca041e66fd9
Sasa Stublic <sasa.stublic@gmail.com>
2021-03-09 19:17:57 +0100

    Added code comments.

--- src/Rhetos.Host.AspNet.RestApi/RestApiApplicationBuilderExtensions.cs
+++ src/Rhetos.Host.AspNet.RestApi/RestApiApplicationBuilderExtensions.cs
@@ -14,6 +14,10 @@ namespace Microsoft.AspNetCore.Builder
 {
     public static class RestApiApplicationBuilderExtensions
     {
+        /// <summary>
+        /// Add discovered Rhetos controllers to MVC feature pipeline and maps them to their respective routes.
+        /// Call before 'app.UseRouting()'.
+        /// </summary>
         public static IApplicationBuilder UseRhetosRestApi(this IApplicationBuilder app)
         {
             var controllerRestInfoRepository = app.ApplicationServices.GetService<ControllerRestInfoRepository>();
@@ -24,6 +28,9 @@ namespace Microsoft.AspNetCore.Builder
                     $"Can't resolve {nameof(ControllerRestInfoRepository)} from dependency injection. Did you forget to 'AddRhetos().AddRestApi()' in ConfigureServices of Startup.cs file?");
             }
 
+            // Slightly hacky way to modify features and conventions AFTER service provider has already been built
+            // Due to this, it is important that this method is executed prior to any others that force feature enumeration (e.g. app.UseRouting())
+            // Also, due to inner workings of MVC, this method will not work if controllers are in 'AsServices' mode (via services.AddControllersAsServices())
             var mvcOptions = app.ApplicationServices.GetRequiredService<IOptions<MvcOptions>>();
             var restApiOptions = app.ApplicationServices.GetRequiredService<IOptions<RestApiOptions>>();
             mvcOptions.Value.Conventions.Add(new RestApiControllerRouteConvention(restApiOptions.Value, controllerRestInfoRepository));
