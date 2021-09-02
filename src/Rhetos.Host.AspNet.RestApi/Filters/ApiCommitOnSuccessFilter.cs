379219f82cc4139c26be963fd27502532cfb9325
Bojan Antolović <bojan.antolovic@gmail.com>
2021-09-02 17:56:25 +0200

    Using IUnitOfWork instead of IPersistenceTransaction.

--- src/Rhetos.Host.AspNet.RestApi/Filters/ApiCommitOnSuccessFilter.cs
+++ src/Rhetos.Host.AspNet.RestApi/Filters/ApiCommitOnSuccessFilter.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/*
+    Copyright (C) 2014 Omega software d.o.o.
+
+    This file is part of Rhetos.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Rhetos.Persistence;
 
@@ -6,12 +25,12 @@ namespace Rhetos.Host.AspNet.RestApi.Filters
 {
     public class ApiCommitOnSuccessFilter : IActionFilter, IOrderedFilter
     {
-        private readonly IRhetosComponent<IPersistenceTransaction> rhetosPersistenceTransaction;
+        private readonly IRhetosComponent<IUnitOfWork> rhetosUnitOfWork;
         public int Order { get; } = int.MaxValue - 20;
 
-        public ApiCommitOnSuccessFilter(IRhetosComponent<IPersistenceTransaction> rhetosPersistenceTransaction)
+        public ApiCommitOnSuccessFilter(IRhetosComponent<IUnitOfWork> rhetosUnitOfWork)
         {
-            this.rhetosPersistenceTransaction = rhetosPersistenceTransaction;
+            this.rhetosUnitOfWork = rhetosUnitOfWork;
         }
 
         public void OnActionExecuting(ActionExecutingContext context)
@@ -21,10 +40,9 @@ namespace Rhetos.Host.AspNet.RestApi.Filters
         public void OnActionExecuted(ActionExecutedContext context)
         {
             if (context.HttpContext.Response.StatusCode == 200 && context.Exception == null)
-            {
-                rhetosPersistenceTransaction.Value.CommitChanges();
-            }
-            rhetosPersistenceTransaction.Value.Dispose();
+                rhetosUnitOfWork.Value.CommitAndClose();
+            else
+                rhetosUnitOfWork.Value.RollbackAndClose();
         }
     }
 }
