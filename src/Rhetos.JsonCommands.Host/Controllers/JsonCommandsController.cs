9f40949b410f7dea359db92a482eaad85efb3d7b
kfranic1 <karlofranic0@gmail.com>
2024-03-18 15:24:49 +0100

    Added `Read` option for `JsonCommands`

--- src/Rhetos.JsonCommands.Host/Controllers/JsonCommandsController.cs
+++ src/Rhetos.JsonCommands.Host/Controllers/JsonCommandsController.cs
@@ -4,6 +4,7 @@ using Rhetos;
 using Rhetos.Dom;
 using Rhetos.Dom.DefaultConcepts;
 using Rhetos.JsonCommands.Host.Filters;
+using Rhetos.JsonCommands.Host.Utilities;
 using Rhetos.Processing;
 using Rhetos.Processing.DefaultCommands;
 using System;
@@ -25,11 +26,13 @@ namespace Rhetos.JsonCommands.Host.Controllers
     {
         private readonly IDomainObjectModel _dom;
         private readonly IProcessingEngine _processingEngine;
+        private readonly IRhetosComponent<GenericFilterHelper> _genericFilterHelper;
 
-        public JsonCommandsController(IRhetosComponent<IDomainObjectModel> dom, IRhetosComponent<IProcessingEngine> processingEngine)
+        public JsonCommandsController(IRhetosComponent<IDomainObjectModel> dom, IRhetosComponent<IProcessingEngine> processingEngine, IRhetosComponent<GenericFilterHelper> genericFilterHelper)
         {
             _dom = dom.Value;
             _processingEngine = processingEngine.Value;
+            _genericFilterHelper = genericFilterHelper;
         }
 
         [HttpPost("write")]
@@ -53,6 +56,51 @@ namespace Rhetos.JsonCommands.Host.Controllers
                 _processingEngine.Execute(saveEntityCommand);
             }
             return Ok();
+        }
+
+        [HttpPost("read")]
+        public IActionResult Read(List<Dictionary<string, JObject>> commands)
+        {
+            List<ReadCommandResult> results = new List<ReadCommandResult>();
+            foreach (var commandDict in commands)
+            {
+                var command = commandDict.Single(); // Each command is deserialized as a dictionary to simplify the code, but only one key-value pair is allowed.
+                string entityName = command.Key;
+                Type entityType = _dom.GetType(entityName);
+                JObject properties = command.Value;
+
+                bool readRecords = properties.GetValue("ReadRecords").Value<bool>();
+                bool readTotalCount = properties.GetValue("ReadTotalCount").Value<bool>();
+
+                bool hasFilter = properties.TryGetValue("Filters", out var filters);
+                bool hasSort = properties.TryGetValue("Sort", out var sort);
+                bool hasSkip = properties.TryGetValue("Skip", out var skip);
+                bool hasTop = properties.TryGetValue("Top", out var top);
+
+                var readEntityCommand = new ReadCommandInfo
+                {
+                    DataSource = entityName,
+                    Filters = hasFilter
+                        ? new QueryParameters(_genericFilterHelper)
+                            .ParseFilterParameters("", "", "", filters.ToString(), entityName)
+                        : Array.Empty<FilterCriteria>(),
+                    OrderByProperties = hasSort
+                        ? sort
+                            .Values<string>()
+                            .Select((e) => new OrderByProperty() { 
+                                Property = e.StartsWith('-') ? e.Substring(1) : e,
+                                Descending = e.StartsWith('-')
+                            })
+                            .ToArray()
+                        : Array.Empty<OrderByProperty>(),
+                    ReadRecords = readRecords,
+                    ReadTotalCount = readTotalCount,
+                    Skip = hasSkip ? skip.Value<int>() : 0,
+                    Top = hasTop ? top.Value<int>() : 0,
+                };
+                results.Add(_processingEngine.Execute(readEntityCommand));
+            }
+            return Ok(results);
         }
 
         private class WriteCommandItems<T> where T : IEntity
