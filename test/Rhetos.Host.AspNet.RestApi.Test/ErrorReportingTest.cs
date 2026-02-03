be65717c15f35096c093323eb5ad80c76232a10a
Bojan Antolović <bojan.antolovic@gmail.com>
2026-02-03 15:14:51 +0100

    Detailed info on flaky unit test.

--- test/Rhetos.Host.AspNet.RestApi.Test/ErrorReportingTest.cs
+++ test/Rhetos.Host.AspNet.RestApi.Test/ErrorReportingTest.cs
@@ -170,9 +170,12 @@ namespace Rhetos.Host.AspNet.RestApi.Test
                 $"rest/TestAction/ReturnServerError/?filters=[{{\"Filter\":\"Guid[]\",\"Value\":{JsonSerializer.Serialize(ids)}}}]");
             string responseContent = await response.Content.ReadAsStringAsync();
 
+            string responseReport = $"{(int)response.StatusCode} {responseContent}";
+            output.WriteLine(responseReport);
+
             Assert.StartsWith
                 ("500 {\"UserMessage\":null,\"SystemMessage\":\"Internal server error occurred. See server log for more information. (ArgumentException, " + DateTime.Now.ToString("yyyy-MM-dd"),
-                $"{(int)response.StatusCode} {responseContent}");
+                responseReport);
 
             output.WriteLine(string.Join(Environment.NewLine, logEntries));
             string[] exceptedLogPatterns = new[] {
